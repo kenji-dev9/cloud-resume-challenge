f117664ec20f4569b2e4738b1ffa24710d5cdae4
kenji-dev9 <kenjiurrutias@gmail.com>
2026-03-05 13:07:48 -0300

    Agregando reglas de seguridad al bucket de S3

--- main.tf
+++ main.tf
@@ -7,3 +7,13 @@ provider "aws" {
 resource "aws_s3_bucket" "mi_curriculum" {
   bucket = "tu-nombre-unico-de-bucket-2026" # Cámbialo por algo único
 }
+
+# Bloqueamos el acceso público a nivel de Bucket (Buenas prácticas de Seguridad)
+resource "aws_s3_bucket_public_access_block" "seguridad_bucket" {
+  bucket = aws_s3_bucket.mi_curriculum.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
